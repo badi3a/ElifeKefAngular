cdf0a0b0c4533d0a623840d68498ebc54c598667
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-10-21 16:09:34 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListProductComponent } from './list-product/list-product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
