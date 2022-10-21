bcf2ad32f49e4a67bb75d1c40b77d48c94aa696c
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-10-21 17:01:35 +0200

    list Product

--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../model/product';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-product.component.css']
 })
 export class ListProductComponent implements OnInit {
-
+  public title: string;
+  public list: Product[];
   constructor() { }
-
   ngOnInit(): void {
+    this.title = 'New App';
+    this.list=[
+      {
+        id: 12,
+        name: 'T-shirt 1',
+        price: 100,
+        nbrLike: 10,
+        quantity: 0,
+        picture: 'https://www.exist.com.tn/61485-large_default/t-shirt.jpg'
+      },
+      {
+        id: 13,
+        name: 'T-shirt 2',
+        price: 130,
+        nbrLike: 90,
+        quantity: 10,
+        picture: 'https://www.exist.com.tn/81822-large_default/t-shirt-de-sport.jpg'
+      }
+    ]
   }
 
 }
